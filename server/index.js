4148092ebe32d944a90230c396f935d38d6de982
dhananjay-yadav-it <dhananjayyadav1807@gmail.com>
2024-12-31 20:53:30 +0530

    add server

--- server/index.js
+++ server/index.js
@@ -13,11 +13,6 @@ app.use(express.json());
 app.use('/api/authentication', require('./routes/user.routes'));
 app.use('/api/appointment', require('./routes/appointment.routes'));
 
-app.get('/',(req,res)=>{
-    res.send('hellow');
-    console.log("hellow");
-});
-
 app.listen(port,()=>{
    console.log(`app listen at http://localhost:${port} `);
 })
